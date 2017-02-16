54f480d7e43b9ef56a64c21320f2145c3ae74880
Michael Noronha <michaeltnoronha@gmail.com>
2017-02-15 18:43:39 -0600

    All testcases terminate, need to correct max

--- ciq_tree/tree.c
+++ ciq_tree/tree.c
@@ -83,14 +83,11 @@ boolMaxTuple maxTraverseChildren(maxLL* toCheck, node* b){
     int newMax;
     do{
         int sLower = toCheck->source ? 1 : 0;
-        /* printf("at %d, about to check UP TO %d children\n",toCheck->node->id,toCheck->node->numChildren-sLower); */
         for(int i=0,childrenFound=0; childrenFound < toCheck->node->numChildren - sLower; ++i){
             sLower = toCheck->source ? 1 : 0;
-            /* printf("slower: %d\n",sLower); */
             if((toCheck->node->children[i] && toCheck->node->children[i] != toCheck->source)
                     || (toCheck->node->children[i] && !toCheck->source)){
                 ++childrenFound;
-                printf("found child of %d, %d\n",toCheck->node->id, toCheck->node->children[i]->id);
                 newMax = toCheck->node->children[i]->n >= toCheck->max ? toCheck->node->children[i]->n : toCheck->max;
                 if(toCheck->node->children[i] == b){
                     ret.max = newMax;
@@ -99,24 +96,18 @@ boolMaxTuple maxTraverseChildren(maxLL* toCheck, node* b){
                 }
                 else if(toCheck->node->children[i]->children){
                     last = addToQueue(last,toCheck->node->children[i],newMax);
-                    /* printf("added to queue, "); */
                     /* printf("current queue: \n"); */
                     /* printCurrentQueue(toCheck); */
                 }
              }
         }
-        /* printf("finished termination of for loop in traversal\n"); */
         if(toCheck->next){
-            /* printf("there's more!\n"); */
-            /* printf("tocheck next node: %d\n",toCheck->next->node->id); */
             toCheck = toCheck->next;
         }
         else
             toCheck = NULL;
-        /* printf("about to run again or fail\n"); */
     }while(toCheck);
-    /* printf("made it to parent traversal\n"); */
-    /* printf("parent pointer: %p\n",first->parent); */
+
     return ret;
 }
 
@@ -125,14 +116,10 @@ void freeMaxLL(maxLL* loc)
    maxLL* tmp;
    int count = 0;
    while (loc->next != NULL){
-       /* ++count; */
        tmp = loc;
        loc= loc->next;
-       /* printf("node within link being freed: %d  ",tmp->max); */
-       /* printf("\n"); */
        free(tmp);
    }
-   /* printf("\nsuccessfully free %d pointers\n",count); */
 }
 
 void add(node* n, int val){
@@ -163,25 +150,17 @@ int maxNode(node* a, node* b){
     if(result.found){
         return result.max;
     }
-    printf("child traversal finished\n");
 
     do{
-        printf("parent we're about to go to: %d\n",toCheck->node->parent->id);
         int newMax = toCheck->node->n >= toCheck->node->parent->n ? toCheck->node->n : toCheck->node->parent->n;
         if(toCheck->node->parent == b){
-            printf("found!!\n");
             return newMax;
         }
-        printf("toCheck node : %d   ",toCheck->node->id);
         temp = toCheck->node;
         toCheck = addToQueue(toCheck,toCheck->node->parent,newMax);
         toCheck->source = temp;
-        printf("toCheck node: %d  ",toCheck->node->id);
-        printf("toCheck->source id: %d",toCheck->source->id);
-        printf("\n");
         result = maxTraverseChildren(toCheck,b);
         if(result.found){
-            printf("found!!\n");
             return result.max;
         }
     }while(toCheck->node->parent);
@@ -218,15 +197,6 @@ int main(){
     sepParChild(NULL,&node_arr[0]);
     node_arr[0].parent=NULL;
 
-    /* for(int i = 0; i < numNodes; ++i){ */
-    /*     printf("%d:  %p  ",i,&node_arr[i]); */
-    /* } */
-    /* printf("\n"); */
-    /* for(int i = 0; i < numNodes; ++i){ */
-    /*     printf("%d: %p  ",i,node_arr[i].parent); */
-    /* } */
-    /* printf("\n"); */
-
     int numQueries, arg1, arg2;
     scanf("%d",&numQueries);
     char query[4];
@@ -237,7 +207,6 @@ int main(){
             add(&node_arr[arg1-1],arg2);
         }
         else{
-            printf("max %d %d\n",arg1,arg2);
             printf("%d\n",maxNode(&node_arr[arg1-1],&node_arr[arg2-1]));
         }
     }
