624bb17de231a1457b376597921f4ff9efd5cc44
Michael Noronha <michaeltnoronha@gmail.com>
2017-02-17 23:26:08 -0600

    Some first icpc stuff

---
+++ kattis/w1/areal.c
@@ -0,0 +1,10 @@
+// Difficulty: 1.5
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    long long a;
+    scanf("%lld",&a);
+    printf("%.20g",4*sqrt(a));
+}
---
+++ kattis/w1/twostones.c
@@ -0,0 +1,12 @@
+// Difficulty: 1.2
+
+#include <stdio.h>
+
+int main(){
+    int result;
+    scanf("%d",&result);
+    if(result%2 == 0)
+        printf("Bob");
+    else
+        printf("Alice");
+}
